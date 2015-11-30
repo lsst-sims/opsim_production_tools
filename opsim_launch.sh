e56bc9d4883e2ccb6c5a415ce5036d23dbae1737
Catherine Petry <petry@ursula.lsst.local,\032lsst.org>
2015-11-30 10:03:11 -0700

    Saving new files.

--- opsim_launch.sh
+++ opsim_launch.sh
@@ -25,7 +25,8 @@ fi
 # maf_watcher.py - cron job to start & monitor MAF
 #################
 # Assumptions
-# - this script will be called from machines ops1, ops2, enigma, minion, ursula, etc.
+# - this script will be called from machines ops1, ops2, enigma, minion,
+#   ursula, etc.
 # - configuration files for each machine will exist (as part of installation)
 #   in /usr/local/etc/conf/opsim/launch.conf (symlink to hostname-launch.conf)
 # - mysql should be running  (mysqld start in .cshrc)
@@ -42,44 +43,46 @@ fi
 # ${HOME}/opsim-db/etc/init.d/mysqld start
 #
 # Configs:
-# Configuration parameters needed by this script are stored in /local/usr/bin/machine.conf
+# Configuration parameters needed by this script are stored in
+# /local/usr/bin/machine.conf
 # There is a configuration file for each machine and each installation of lsst
 #      ???? ask Michael about multiple versions and file location
-# source /usr/local/etc/opsim/launch.conf  # NOTE: this is a symlink to hostname-launch.conf
+# source /usr/local/etc/opsim/launch.conf  # NOTE: this is a symlink to
+# hostname-launch.conf
 source ./launch.conf  # testing
 
-#make sure machine name is lowercase
-opsim_machine=$(echo $OPSIM_HOSTNAME | awk '{print tolower($0)}')
-
 # User reviews parameters in all configuration files (/lsst/opsim_3_x_y/conf/*).
-# User reviews file names specified in LSST.conf (or other name specified by --config).
+# User reviews file names specified in LSST.conf
+# (or other name specified by --config).
 # User needs ssh keys set up between relevant machines.
 printf "\n"
 printf "%s\n" "Please note that:"
-printf "%s\n" "     1 - You have copied the \$SIMS_OPERATIONS_DIR/conf structure to your"
-printf "%s\n" "         \$RUN_DIR".
-printf "%s\n" "     2 - You have made any changes to LSST.conf and configuration files"
-printf "%s\n" "         specified therein."
-printf "%s\n" "     3 - You have ssh keys set up between the machine you are running on"
-printf "%s %s%s\n" "         on and the repository machine, " $REPO_MACHINE "."
+printf "%s\n" "    1 - You have copied the \$SIMS_OPERATIONS_DIR/conf structure"
+printf "%s\n" "        to your \$RUN_DIR".
+printf "%s\n" "    2 - You have made any changes to LSST.conf and configuration"
+printf "%s\n" "        files specified therein."
+printf "%s\n" "    3 - You have ssh keys set up between the machine you are"
+printf "%s " "        running on and the repository machine"
+printf ", %s.\n" $REPO_MACHINE
 printf "\n"
 
 
 # Setup EUPS
-printf "source %s\n" ${LSST_HOME}/loadLSST.bash
+printf "Sourcing %s.\n\n" ${LSST_HOME}/loadLSST.bash
 source ${LSST_HOME}/loadLSST.bash
-printf "%s\n" $EUPS_DIR
+#printf "%s\n" $EUPS_DIR
 
 # Setup sims_operations
 # from $version - create OPSIM_TAG and OPSIM_DIR
-# $version & $config will be read in as an argument; set here for testing purposes
+# $version & $config will be read in as an argument;
+# set here for testing purposes
 version=3.3.3             # for testing
 
 opsim_dir="opsim_"${version}
 # create the eups tag from the directory name by substituting "." with "_"
 opsim_tag=$(echo $opsim_dir | sed 's/\./_/g')
 
-printf "OpSim will be executing with the version tagged %s\n" ${opsim_tag}
+printf "OpSim will be executing with the version tagged %s.\n\n" ${opsim_tag}
 setup sims_operations -t ${opsim_tag}
 
 # Determine sessionID for this simulation
@@ -89,18 +92,18 @@ session_id=`mysql -u www --password=zxcvbnm --skip-column-names -e "select max(S
 if [ "$session_id" = "NULL" ] ; then
    session_id="1000"
 fi
-printf "This session ID will be %s_%d\n" ${opsim_machine} ${session_id}
+printf "This session ID will be %s_%d.\n\n" ${OPSIM_HOSTNAME} ${session_id}
 
 config=conf/survey/LSST.conf          # for testing as input in argument
 # Save configuration files called by opsim.py to repository machine
 # Set up distribution machine directories
-repo_conf_dir=${REPO_PATH}/${opsim_machine}_${session_id}/conf
+repo_conf_dir=${REPO_PATH}/${OPSIM_HOSTNAME}_${session_id}/conf
 ssh ${REPO_MACHINE} "mkdir -p ${repo_conf_dir}/system ${repo_conf_dir}/survey ${repo_conf_dir}/scheduler"
 #
 #scp -r ./conf ops2.lsst.org:$confdir &   # this runs in background; copies ALL files/directories in ./conf
 #
 # Read LSST.conf (or other name specified by --config) to select only the configuation files that are used.
-printf "%s\n" "The following files will be saved to the repository:"
+printf "The following files will be saved to %s:%s:\n\n" ${REPO_MACHINE} ${repo_conf_dir}
 
 survey_confs=$(sed '/^#/d' ${RUN_DIR}/${opsim_dir}/$config | sed '/survey.*conf/!d' | sed 's/^.*= //' | \
              awk -v a=${RUN_DIR} -v b=${opsim_dir} '{printf "%s/%s/%s ", a, b, $1}')
@@ -109,16 +112,21 @@ system_confs=$(sed '/^#/d' ${RUN_DIR}/${opsim_dir}/$config | sed '/system.*conf/
 scheduler_confs=$(sed '/^#/d' ${RUN_DIR}/${opsim_dir}/$config | sed '/scheduler.*conf/!d' | sed 's/^.*= //' | \
              awk -v a=${RUN_DIR} -v b=${opsim_dir} '{printf "%s/%s/%s ", a, b, $1}')
 #
-printf "%s\n" $survey_confs
-printf "%s\n" $system_confs
-printf "%s\n" $scheduler_confs
+#printf "%s\n" $survey_confs
+#printf "%s\n" $system_confs
+#printf "%s\n" $scheduler_confs
 
-# Only needs -r (recursive) to copy survey/system/scheduler dirs (not files)
-printf "%s\n"
-printf "%s\n" ${REPO_MACHINE}:${repo_conf_dir}/survey
-scp $survey_confs ${REPO_MACHINE}:${repo_conf_dir}/survey
-scp $system_confs ${REPO_MACHINE}:${repo_conf_dir}/system
-scp $scheduler_confs ${REPO_MACHINE}:${repo_conf_dir}/scheduler
+printf "%s\n" ${survey_confs}
+scp ${survey_confs} ${REPO_MACHINE}:${repo_conf_dir}/survey
+
+printf "%s\n" ${system_confs}
+scp ${system_confs} ${REPO_MACHINE}:${repo_conf_dir}/system
+
+printf "%s\n" ${scheduler_confs}
+scp ${scheduler_confs} ${REPO_MACHINE}:${repo_conf_dir}/scheduler
+
+printf "\n"
+
 
 ##########
 ### Start simulation
@@ -127,18 +135,27 @@ scp $scheduler_confs ${REPO_MACHINE}:${repo_conf_dir}/scheduler
 # usage: opsim.py [--profile=yes] [--verbose=yes] [--track=no] [--config=conf/survey/LSST.conf] [--startup_comment="comment"]
 #background? redirect stdout and stderr
 # check for $# that startup comment exists
-### opsim.py --config=conf/survey/LSST.conf --startup_comment="${startup_comment}" >& log/${session_id}.log &
+#opsim.py --config=conf/survey/LSST.conf --startup_comment="${startup_comment}" >& log/${session_id}.log &
 # capture process id here  newid=$!
+./sim_opsim.py &
+process_id=$!
+ps aux | grep sim_opsim
+echo sim_opsim is $process_id
+printf "as an integer %d\n\n" $process_id
+which python
+echo that was python
+
 
 #   wait for about 10 min to make sure opsim.py has done all it's preparatory work and nRun is populated
-sleep 3   # change to 600 for 10 minutes; 7sec is for testing
+printf "Waiting for Godot...\n\n"
+sleep 3   # change to 600 for 10 minutes; 3sec is for testing
 
 #   capture job-id and process-id from opsim.py and run opsim_monitor.py
-printf "%s\n" "Starting Monitor"  #NOTE: install in and call from  /usr/local/bin/opsim_monitor.py
-#./opsim_monitor.py newid &
+printf "%s\n\n" "Starting Monitor"  #NOTE: install in and call from  /usr/local/bin/opsim_monitor.py
+./opsim_monitor.py ${process_id} &
 
 # Cleanup
-printf "%s\n" "Ending opsim_launch.sh"
+printf "%s\n\n" "Ending opsim_launch.sh"
 exit 0
 
 
--- opsim_monitor.py
+++ opsim_monitor.py
@@ -1,48 +1,82 @@
 #!/usr/bin/python
-#################
-# opsim_monitor.py
-#
-# Called from opsim_launch.sh to monitor the progress of the simulation
-# Calls opsim_postprocess.py
-# Usage: opsim_launch.sh "my startup comment"
-#
+"""
+ opsim_monitor.py
+ Usage : opsim_monitor.py pid
+
+ Called from opsim_launch.sh to monitor the progress of the simulation
+ Calls opsim_postprocess.py
+"""
+
 import sys
 import os
+import time
+
 #import opsim_postprocess
 from opsim_postprocess import postprocess
+  
+def check_pid(pid):        
+    """ Check For the existence of a unix pid. """
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
-   print("\n")
-   print("Hello World\n")
-   print("This is where the simulation is monitored.\n")
-   print("\n")
+    
+    print("This is opsim_monitor.py\n")
+
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage : %s process_id" % sys.argv[0])
+        raise SystemExit(1)
+    process_id = sys.argv[1]
+    
+    while check_pid(int(process_id)):
+        print("This process exists")  # email the message to opsim-prod@lists.lsst.org
+        time.sleep(5)
+    else:
+        print("This process does not exist")
+        
+    # test that the number of nights achieved corresponds to nights requested.
+    #set maxnights
+    #set nRun
+    if max_nights != nRun:
+        print("There is a problem that needs to be tracked down")
+        #find the traceback error
+    else:
+        print("The simulation has completed successfully")
+        opsim_postprocess()
+
+
 #  After the simulation ends then
 #  opsim_postprocess()
-#   os.system('python opsim_postprocess.py')
+#  os.system('python opsim_postprocess.py')
+#  postprocess()
+
+sys.exit(0)
 
 
-   postprocess()
+"""
    
-   sys.exit(0)
-   
-# send out email message at start
-#   every 15min, check the progress (max(Session.night) compared to Session.nRun in nights) 
-#   and send a message to opsim-prod@lists.lsst.org
-#         e.g. subject could be "sessionID launched"; "sessionID finished"; "sessionID processed"
-#         add in errors later
-#   terminate this job when the simulation is complete ->
-#   process id doesn't exist && nRun should be maxNights   Error if pid is not there and maxNights
+send out email message at start
+   every 15min, check the progress (max(Session.night) compared to Session.nRun in nights) 
+   and send a message to opsim-prod@lists.lsst.org
+         e.g. subject could be "sessionID launched"; "sessionID finished"; "sessionID processed"
+         add in errors later
+   terminate this job when the simulation is complete ->
+   process id doesn't exist && nRun should be maxNights   Error if pid is not there and maxNights
 while pid.exists()
 keep checking
   
   check maxnights != runN
    send out an error message (exerpt log file error - grep for Traceback, return line number, open file (not read it) and read from that line to the end and print - and print out)  and exit
    or run postprocessing as a callable
-   # print section of file from regular expression to end of file
+  # print section of file from regular expression to end of file
  sed -n '/regexp/,$p'
    
-#   IF there were observations on the last night
-#   maybe check TimeHistory instead
-#
-#   could fail if the process-id ends & the last night is not reached (nRun)
-    
+   IF there were observations on the last night
+   maybe check TimeHistory instead
+
+   could fail if the process-id ends & the last night is not reached (nRun)
+"""
